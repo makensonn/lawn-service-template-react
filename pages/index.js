7064df1d194c9c2fb21896bb015c1de5e386b53d
Makenson Noel <22712773+makensonn@users.noreply.github.com>
2023-07-29 15:05:48 +0000

    Index update

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Lucien Lawn Service| Freshly Cut Lawns</title>
+        <title>Lucien Lawn Service | Freshly Cut Lawns</title>
         <meta name="description" content="If you're looking for excelent lawn service, please don't hesitate to reach out." />
         <link rel="icon" href="/fav.png" />
       </Head>
